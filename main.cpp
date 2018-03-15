cbbe1bf5790f05360e2e19a5a70a8df3be4f0092
Thomas Di Giusto <thomasdigiusto@me.com>
2018-03-15 16:44:26 +0100

    Note a moi meme

--- main.cpp
+++ main.cpp
@@ -23,3 +23,23 @@ int main(){
 
   return 0;
 }
+
+/*
+  Bibliotheque:
+  http://www-inf.int-evry.fr/cours/C++/CoursEA/node33.html
+  http://www.cplusplus.com/doc/tutorial/classes/
+  https://isocpp.org/wiki/faq/classes-and-objects
+*/
+
+
+/*
+  Continuer sur Git:
+  https://git-scm.com/book/fr/v2/Les-bases-de-Git-Travailler-avec-des-d%C3%A9p%C3%B4ts-distants
+*/
+
+
+/*
+  Note a moi meme:
+  /export/home/thomas/baltiks/test_baltik/src
+  emacs -nw Op_Diff_VEF_Face.cpp
+*/
